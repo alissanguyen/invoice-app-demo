0fe49ace07d9c9be29f5795b17f12b8233e1b1c9
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2022-01-23 23:04:14 -0800

    Install dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import { Route, Switch, useLocation } from "react-router-dom";
+import Wrapper from "./components/Wrapper/Wrapper";
+import Header from "./components/Header/Header";
+import Invoices from "./components/Invoices/Invoices";
+import FormController from "./components/FormController/FormController";
+import InvoiceView from "./components/InvoiceView/InvoiceView";
+import Modal from "./components/Modal/Modal";
+import RouteError from "./components/RouteError/RouteError";
+import { useGlobalContext } from "./components/App/context";
+import { AnimatePresence } from "framer-motion";
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  const { state } = useGlobalContext();
+  const isModalOpen = state.isModalOpen.status;
+  const isFormOpen = state.isFormOpen;
+  const location = useLocation();
+
+  return (
+    <div className="App">
+      <Wrapper>
+        <Header />
+        <AnimatePresence>
+          {isFormOpen && <FormController />}
+          {isModalOpen && <Modal />}
+        </AnimatePresence>
+        <AnimatePresence exitBeforeEnter>
+          <Switch location={location} key={location.key}>
+            <Route exact path="/">
+              <Invoices />
+            </Route>
+            <Route path="/invoice/:id" children={<InvoiceView />} />
+            <Route path="*">
+              <RouteError />
+            </Route>
+          </Switch>
+        </AnimatePresence>
+      </Wrapper>
+    </div>
+  );
+};
 
 export default App;
--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -1,60 +1,60 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef } from "react";
 
-const initialFilterType = 'all';
+const initialFilterType = "all";
 
 const useFilter = (callback) => {
-    const [filteredInvoices, setFilteredInvoices] = useState(callback.invoices);
-    const [filterType, setFilterType] = useState(initialFilterType);
-    const previousFilterType = useRef(initialFilterType);
+  const [filteredInvoices, setFilteredInvoices] = useState(callback.invoices);
+  const [filterType, setFilterType] = useState(initialFilterType);
+  const previousFilterType = useRef(initialFilterType);
 
-    /**
-     * Running an effect whenever callback changes and call handleFilter() with current type
-     */
-    useEffect(() => {
-        handleFilter(filterType);
-    }, [callback]);
+  /**
+   * Running an effect whenever callback changes and call handleFilter() with current type
+   */
+  useEffect(() => {
+    handleFilter(filterType);
+  });
 
-    /**
-     * Function to filter filteredInvoices based on filter type.
-     * @param    {String}  string - String with filter type
-     */
-    const handleFilter = (type) => {
-        if (type === initialFilterType) {
-            setFilteredInvoices(callback.invoices);
-            return false;
-        }
+  /**
+   * Function to filter filteredInvoices based on filter type.
+   * @param    {String}  string - String with filter type
+   */
+  const handleFilter = (type) => {
+    if (type === initialFilterType) {
+      setFilteredInvoices(callback.invoices);
+      return false;
+    }
 
-        const newInvoices = callback.invoices.filter(
-            (item) => item.status === type
-        );
-        setFilteredInvoices(newInvoices);
-    };
+    const newInvoices = callback.invoices.filter(
+      (item) => item.status === type
+    );
+    setFilteredInvoices(newInvoices);
+  };
 
-    /**
-     * Function to change filter type based on passed props.
-     * Call handleFilter function to change filteredInvoices based on that filter type.
-     * @param    {String}  event - String with filter type
-     */
-    const changeFilterType = (event) => {
-        const type = event.target.value;
+  /**
+   * Function to change filter type based on passed props.
+   * Call handleFilter function to change filteredInvoices based on that filter type.
+   * @param    {String}  event - String with filter type
+   */
+  const changeFilterType = (event) => {
+    const type = event.target.value;
 
-        if (previousFilterType.current === type) {
-            previousFilterType.current = initialFilterType;
-            handleFilter(initialFilterType);
-            setFilterType(initialFilterType);
-            return false;
-        }
+    if (previousFilterType.current === type) {
+      previousFilterType.current = initialFilterType;
+      handleFilter(initialFilterType);
+      setFilterType(initialFilterType);
+      return false;
+    }
 
-        previousFilterType.current = type;
-        handleFilter(type);
-        setFilterType(type);
-    };
+    previousFilterType.current = type;
+    handleFilter(type);
+    setFilterType(type);
+  };
 
-    return {
-        filteredInvoices,
-        filterType,
-        changeFilterType,
-    };
+  return {
+    filteredInvoices,
+    filterType,
+    changeFilterType,
+  };
 };
 
 export default useFilter;
